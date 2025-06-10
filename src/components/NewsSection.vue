016626f6a9e1b45600fc65e3b444469d47586378
xAksssenov <aks.kirill32@gmail.com>
2025-06-10 17:18:43 +0300

    fix: problems

--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -7,13 +7,14 @@
                 <router-link to="/news" class="news__more-link">ВСЕ НОВОСТИ</router-link>
             </div>
             <ul class="news__list">
-                <li class="news__item" v-for="post in newsStore.news" :key="post.date">
+                <li class="news__item" v-for="post in newsStore.news" :key="post.created_at">
                     <router-link :to="{ name: 'NewsDetail', params: { id: post.id } }" class="news__link">
-                        <div class="news__image"><img :src="post.photos[0].image" alt="" class="news__icon" /></div>
+                        <div class="news__image" v-if="post.photos"><img :src="post.photos[0].image" alt=""
+                                class="news__icon" /></div>
                         <div class="news__container">
-                            <p class="news__date">{{ post.date }}</p>
+                            <p class="news__date">{{ post.created_at }}</p>
                             <h3 class="news__subtitle">{{ post.title }}</h3>
-                            <p class="news__description">{{ post.shortDescription }}</p>
+                            <p class="news__description">{{ post.text }}</p>
                         </div>
                     </router-link>
                 </li>
